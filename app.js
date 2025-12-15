d38e184d2d94efc45ea7b4a652e0b9b972009863
Azat Gal <azatgt1996@yandex.ru>
2025-12-15 22:55:37 +0300

    add slow mode, add audio files

--- app.js
+++ app.js
@@ -1,58 +1,77 @@
 const tg = window.Telegram.WebApp
 
-$('#lesson-select').innerHTML = /*html*/ `
-  <option value="0">Алфавит</option>
-  ${lessons.map((it) => /*html*/ `<option value="${it.id}">${it.name}</option>`).join('')}
-`
+$('#lesson-select').innerHTML = lessons
+  .map((it) => /*html*/ `<option value="${it.id}">${it.id}. ${it.name}</option>`)
+  .join('')
 
-$('#alphabet').innerHTML = /*html*/ `
-${alphabetTable}
-<p>${alphabetDescription}</p>`
+$('#alphabet').innerHTML = alphabetContent
 
 function skip(seconds) {
   const { duration, currentTime } = $('#lesson-audio')
   $('#lesson-audio').currentTime = Math.max(0, Math.min(duration, currentTime + seconds))
 }
 
+const setPlaybackRate = () => ($('#lesson-audio').playbackRate = isAudioSlow ? 0.7 : 1)
+
+function slowAudio() {
+  isAudioSlow = !isAudioSlow
+  ls.set('isAudioSlow', isAudioSlow)
+  $cl('#slow-btn').toggle('active')
+  setPlaybackRate()
+}
+
+let isAudioSlow = ls.get('isAudioSlow') === 'true'
+if (isAudioSlow) $cl('#slow-btn').add('active')
+setPlaybackRate()
+
 let hasDescription = ls.get('hasDescription') !== 'false'
 $toggleVisible('#description', hasDescription)
-if (hasDescription) $cl('#description-btn').add('red')
+if (hasDescription) $cl('#description-btn').add('active')
 
 function toggleDescription() {
   hasDescription = !hasDescription
-  $cl('#description-btn').toggle('red')
+  $cl('#description-btn').toggle('active')
   ls.set('hasDescription', hasDescription)
   $toggleVisible('#description', hasDescription)
 }
 
 let hasHint = ls.get('hasHint') !== 'false'
-if (hasHint) $cl('#hint-btn').add('red')
+if (hasHint) $cl('#hint-btn').add('active')
 
-let currentLessonId = ls.get('currentLessonId') ?? '0'
+let lesson = null
+let currentLessonId = ls.get('currentLessonId') ?? ALPHABET_ID
 $('#lesson-select').value = currentLessonId
 selectLesson(currentLessonId)
 
 function toggleHint() {
   hasHint = !hasHint
-  $cl('#hint-btn').toggle('red')
+  $cl('#hint-btn').toggle('active')
   ls.set('hasHint', hasHint)
-  selectLesson(currentLessonId)
+  setLessonWords()
 }
 
 function selectLesson(value) {
   currentLessonId = value
+  $('#lesson-audio').pause()
   ls.set('currentLessonId', currentLessonId)
-  $toggleVisible('#alphabet', value === '0')
-  $toggleVisible('#lesson', value !== '0')
-  if (value === '0') return
+  $toggleVisible('#alphabet', value === ALPHABET_ID)
+  $toggleVisible('#lesson', value !== ALPHABET_ID)
+  if (value === ALPHABET_ID) return
 
-  const lesson = lessons.find((it) => it.id === +value)
-  const letter = lesson.letter
-
+  lesson = lessons.find((it) => it.id === +value)
+  $('#lesson-audio').src = `audio_files/lesson${lesson.id}.mp3`
   $('#description').innerHTML = lesson.description
   $('#intro').innerHTML = lesson.intro
+  setPlaybackRate()
+  setLessonWords()
+}
+
+function setLessonWords() {
   let text = lesson.words ?? ''
-  if (hasHint) text = text.replaceAll(letter, redStr(letter))
+  if (hasHint) {
+    const letter = lesson.letter
+    text = text.replaceAll(letter, redStr(letter))
+  }
   $('#words').innerHTML = text
 }
 
